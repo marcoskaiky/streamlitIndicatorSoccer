126c047773371fa1da9f21470882290f1c460251
Marcos Kaiky Garcia <tecmarcosk@gmail.com>
2025-08-14 16:02:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import os
+import os 
 import pandas as pd
 import psycopg2
 from dotenv import load_dotenv
 import streamlit as st
 import plotly.express as px
+
+#-------------------------------------------------------------------------------
 
 load_dotenv()
 
@@ -15,6 +17,8 @@ def conectar_banco():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD")
     )
+
+#-------------------------------------------------------------------------------
 
 @st.cache_data
 def carregar_dados():
@@ -29,33 +33,61 @@ def carregar_dados():
     conn.close()
     return df
 
+
+#-------------------------------------------------------------------------------
+
+
+df = carregar_dados()
+
 with st.sidebar:
     if st.button("Recarregar Dados"):
         st.cache_data.clear()
-    df_full = carregar_dados()
-    times = ["Todos"] + df_full["time"].unique().tolist()
+        
     st.markdown("---")
-    filtro_time = st.selectbox("Time", times)
+    st.metric("⚽Total de Gols", int(df["gols"].sum()))
+    st.metric("Total de Assistências", int(df["assistencias"].sum()))
+    st.metric("Quantidade de Jogadores", int(df["nome"].nunique()))
+    st.markdown("---")
 
-df = df_full if filtro_time == "Todos" else df_full[df_full["time"] == filtro_time]
-
-k1, k2, k3 = st.columns(3)
-k1.metric("⚽Gols", int(df["gols"].sum()))
-k2.metric("🎯Assistências", int(df["assistencias"].sum()))
-k3.metric("🙋Jogadores", int(df["nome"].nunique()))
-st.markdown("---")
+#-------------------------------------------------------------------------------
 
 top_gols = df.nlargest(10, "gols")
 st.title("Top 10 Jogadores com Mais Gols")
-st.plotly_chart(px.bar(top_gols, x="nome", y="gols", color="time"), use_container_width=True)
+st.plotly_chart(
+    px.bar(
+        top_gols, 
+        x="nome", 
+        y="gols", 
+        color="time"), use_container_width=True)
 st.markdown("---")
 
+#-------------------------------------------------------------------------------
+
 top_ga = df.nlargest(10, "ga")
-st.title("Top 10 Jogadores com Mais Gols e Assistências")
-st.plotly_chart(px.bar(top_ga, x="nome", y="ga", color="time"), use_container_width=True)
+st.title("Saldo G+A")
+fig = px.scatter_3d(
+    top_ga,
+    x="nome", 
+    y="time", 
+    z="ga", 
+    color="time", 
+    size="ga", 
+    hover_name="nome"
+)
+st.plotly_chart(fig, use_container_width=True)
 st.markdown("---")
+
+#-------------------------------------------------------------------------------
 
 gols_por_time = df.groupby("time")["gols"].sum().reset_index()
 st.title("Gols por Time")
-st.plotly_chart(px.pie(gols_por_time, names="time", values="gols"), use_container_width=True)
+st.plotly_chart(
+    px.pie(
+        gols_por_time, 
+        names="time", 
+        values="gols"), 
+    use_container_width=True)
+
 st.markdown("---")
+
+#-------------------------------------------------------------------------------
