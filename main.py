de2f0112b90b5513f25bd09e0716f01e21d46524
Marcos Kaiky Garcia <tecmarcosk@gmail.com>
2025-08-19 10:12:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,93 +1,79 @@
-import os 
+import os
 import pandas as pd
 import psycopg2
 from dotenv import load_dotenv
 import streamlit as st
 import plotly.express as px
 
-#-------------------------------------------------------------------------------
-
 load_dotenv()
 
-def conectar_banco():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
-    )
+def conexao_banco():
+  return psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+    
+  )
 
-#-------------------------------------------------------------------------------
-
-@st.cache_data
+@st.cache_data ###
 def carregar_dados():
-    conn = conectar_banco()
-    df = pd.read_sql("""
-        SELECT j.nome, t.nome AS time, e.gols, e.assistencias,
+  conn = conexao_banco()
+  df = pd.read_sql("""SELECT j.nome, t.nome AS time, e.gols, e.assistencias,
                (e.gols + e.assistencias) AS ga
         FROM jogadores j
         JOIN times t ON j.time_id = t.id
-        JOIN estatisticas e ON j.id = e.jogador_id;
-    """, conn)
-    conn.close()
-    return df
-
-
-#-------------------------------------------------------------------------------
-
+        JOIN estatisticas e ON j.id = e.jogador_id;""",conn)
+  conn.close()
+  return df
 
 df = carregar_dados()
 
 with st.sidebar:
-    if st.button("Recarregar Dados"):
-        st.cache_data.clear()
-        
-    st.markdown("---")
-    st.metric("⚽Total de Gols", int(df["gols"].sum()))
-    st.metric("Total de Assistências", int(df["assistencias"].sum()))
-    st.metric("Quantidade de Jogadores", int(df["nome"].nunique()))
-    st.markdown("---")
+  if st.button("Atualizar Dados"):
+    st.cache_data.clear()
 
-#-------------------------------------------------------------------------------
+  st.markdown("---")
+  st.metric("Total de gols",int(df["gols"].sum()))
+  st.metric("Total de assistencias",int(df["assistencias"].sum()))
+  st.metric("Total de jogadores inscritos",int(df["nome"].nunique())) #####
 
 top_gols = df.nlargest(10, "gols")
-st.title("Top 10 Jogadores com Mais Gols")
+st.title("Top 10 Artilheiros")
 st.plotly_chart(
-    px.bar(
-        top_gols, 
-        x="nome", 
-        y="gols", 
-        color="time"), use_container_width=True)
-st.markdown("---")
+  px.bar(
+    top_gols,
+    x="nome",
+    y="gols",
+    color="time"
+  )
+)
+st.markdown("---")  
 
-#-------------------------------------------------------------------------------
 
 top_ga = df.nlargest(10, "ga")
-st.title("Saldo G+A")
+st.title("TOP 10 saldo G+A")
 fig = px.scatter_3d(
-    top_ga,
-    x="nome", 
-    y="time", 
-    z="ga", 
-    color="time", 
-    size="ga", 
-    hover_name="nome"
+  top_ga,
+  x="nome",
+  y="time",
+  z="ga",
+  color="time",
+  size="ga",
+  hover_name="nome"
 )
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig)
 st.markdown("---")
 
-#-------------------------------------------------------------------------------
 
-gols_por_time = df.groupby("time")["gols"].sum().reset_index()
-st.title("Gols por Time")
+gols_por_time = df.groupby("time")["gols"].sum().reset_index() ###
+st.title("Gols por time")
 st.plotly_chart(
-    px.pie(
-        gols_por_time, 
-        names="time", 
-        values="gols"), 
-    use_container_width=True)
-
-st.markdown("---")
-
-#-------------------------------------------------------------------------------
+  px.pie(
+    gols_por_time,
+    names="time",
+    values="gols",
+    
+  )
+)
